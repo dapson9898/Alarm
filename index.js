41ce5300aaec35c96d4d671199ee0bc03d31cdbf
dapson9898 <83287316+dapson9898@users.noreply.github.com>
2025-11-24 20:22:57 +0100

    fixed a loop bug

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ let time;
 let hoursInput = new Date().getHours() + 1;
 hoursInput = hoursInput > 24 ? hoursInput - 24 : hoursInput;
 const formattedHours = hoursInput < 10 ? "0" + hoursInput : hoursInput;
-const formattedMinutes =
-    new Date().getMinutes() < 10
-        ? "0" + new Date().getMinutes()
-        : new Date().getMinutes();
+const formattedMinutes = new Date().getMinutes() < 10 ? "0" + new Date().getMinutes() : new Date().getMinutes();
 const currentTime = `${formattedHours}:${formattedMinutes}`;
-console.log(currentTime);
+// console.log(currentTime);
 alarmTime.value = currentTime;
 
 function alarmSystem() {
@@ -40,19 +37,21 @@ function displayAllAlarm() {
     } else {
         setAlarmTime.map((alarm, index) => {
             displayAlarms.innerHTML += `<h1>(${index + 1}) ${alarm}</h1>`;
+            console.log("alarm +", alarm)
         });
     }
 }
 
+let myalarmTime = document.getElementById("alarmTime").value
+console.log(myalarmTime)
 function setAlarm() {
-    if ((alarmTime.value = "")) {
+    if ((alarmTime.value == "")) {
         alert("Set alarm time");
         return;
     }
     setAlarmTime.push(alarmTime.value);
     console.log(setAlarmTime);
     repeat = setInterval(alarmSystem, 1000);
-    alarmTime.value = "";
     displayAllAlarm();
     alarmTime.value = currentTime;
 }
